64ef94f3c2130a87e96bd6f0acbebbab8b2bef34
Vaishnavi S <jrsvaishnavi@gmail.com>
2026-04-22 13:59:53 +0530

    Day 1: Environment setup and infrastructure configuration

---
+++ backend/surveytool/src/test/java/com/riskculture/surveytool/SurveytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskculture.surveytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveytoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
